92ef8b17b0fbf0f4dc64acf2974990fe4fdc069f
kjjwhitlock <katiejwhitlock@gmail.com>
2021-02-08 15:57:48 +0000

    failing unit test for top_up method written

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,5 +7,7 @@ describe OysterCard do
   it 'Checks that balance is 0' do
     expect(subject.balance).to eq 0
   end
-
+  it 'enables a top up with an amount chosen by the user' do
+    expect{subject.top_up(20)}.to change {subject.balance}.by(20)
+  end
 end
